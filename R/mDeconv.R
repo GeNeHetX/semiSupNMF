7e4476497330e563f3dec95c2d23b8d0c6acf9be
RemyNicolle <remy.c.nicolle@gmail.com>
2021-12-14 18:48:33 +0100

    Added log by default, CPM normalized wisped and edger dependecy

--- R/mDeconv.R
+++ R/mDeconv.R
@@ -10,15 +10,25 @@
 #'
 #' @examples
 mDeconv <- function(XEXP,gL,addG=NULL,addWhiteComp=F,
-                    maxIter=2000L,nthreads=1,printInfo=F){
+                    maxIter=2000L,nthreads=1,printInfo=F,isLogged2=TRUE){
 
+  #Run everything in log2 scale
+  doLog=TRUE
 
   gL=lapply(gL,intersect,y=rownames(XEXP))
   if(min(sapply(gL,length))<2){
-    stop("At least 2 marker genes should be present in each signature and present in the expression data")
+    stop("At least 2 marker genes  (arg: gL) should be present in each signature and present in the expression data")
   }
   if(max(table(unlist(gL)))>1){
-    stop("Each gene in MArker gene list should only be asscoiated to one phenotype")
+    stop("Each gene in MArker gene list (arg: gL) should only be asscoiated to one phenotype")
+  }
+
+  if(length(gL)>100){
+    stop("More than 100 gene lists seems unreasonable.")
+  }
+
+  if(length(unique(names(gL))) < length(gL)){
+    names(gL)= paste0("geneL",combn(letters,2,paste,collapse=""))[1:length(gL)]
   }
 
 
@@ -34,7 +44,11 @@ mDeconv <- function(XEXP,gL,addG=NULL,addWhiteComp=F,
 
   selg=rownames(MARK)
 
-  X=XEXP[selg,]
+  X=as.matrix(XEXP[selg,])
+
+  if(isLogged2){
+   X=semiSupNMF:::.qcpm(X,isLogged2,doLog)
+  }
   initwmax=(MARK*matrixStats::rowMaxs(X)) #+ ((abs(MARK-1)*matrixStats::rowMins(X)))
 
   nmark=colSums(MARK)
@@ -66,9 +80,9 @@ mDeconv <- function(XEXP,gL,addG=NULL,addWhiteComp=F,
 
 
   mynmf=semiSupNMF:::semiSupNMF(A=X, k = ncol(MARK),
-                   init = list(W=initwmax,H=initHprop), mask = list(W=MARK,H=matrix(F,ncol(MARK),ncol(X))),
-                   check.k = TRUE,max.iter = maxIter, rel.tol = 1e-4, n.threads = nthreads, trace = 0.1,
-                   verbose = 0, show.warning = TRUE, inner.rel.tol = 1e-9)
+                                init = list(W=initwmax,H=initHprop), mask = list(W=MARK,H=matrix(F,ncol(MARK),ncol(X))),
+                                check.k = TRUE,max.iter = maxIter, rel.tol = 1e-4, n.threads = nthreads, trace = 0.1,
+                                verbose = 0, show.warning = TRUE, inner.rel.tol = 1e-9)
 
 
 
@@ -82,12 +96,161 @@ mDeconv <- function(XEXP,gL,addG=NULL,addWhiteComp=F,
   NewW= W %*%D
   NewHtprop=t(NewH)*(1/colSums(NewH))
   colnames(NewHtprop)=colnames(Ht)
-  NewHtprop
-  list(H=Ht,W=W,Hprop=NewHtprop,Wprop=NewW,NMF=mynmf)
+
+  Wnorm=semiSupNMF:::.qcpm(W, unlog=F,outlog=isLogged2)
+
+  normWISP=NULL
+  try({normWISP=semiSupNMF:::.qWISProutine(Wnorm,X)})
+
+  list(H=Ht,W=W,Hp=NewHtprop,Wp=NewW,Wnorm=Wnorm,normWISP=normWISP,NMF=mynmf,expUsed=X)
 
 }
 
 
+
+
+
+
+
+
+
+.qcpm=function(x,unlog=F,outlog=F){
+  if(unlog){
+    x=((2^(x))-1)
+  }
+  y=edgeR::cpm(x, normalized.lib.sizes = TRUE,log = F)
+  if(outlog){y=log2(y+1)}
+  y
+}
+
+
+.qWISProutine=function(W,expdat){
+
+  W=W[,which(colSums(W)>0)]
+
+  if(any(rownames(expdat) != rownames(W))){
+    stop(paste0("In the WISP routine, expression data and gene weight matrix should",
+                " have exactly the same gene/row names in the same order"))
+  }
+
+
+  wisped=NULL
+  subcomps=setdiff(colnames(W),"additional")
+  try({wisped=semiSupNMF:::.qWISP(expdat,W[,subcomps],scaling="none",sum_LessThanOne=T)},silent=T)
+  cpt=0
+  while(is.null(wisped) & length(subcomps)>=2 & cpt <4) {
+    cpt=cpt+1
+    try({
+      subcomps=setdiff(subcomps,subcomps[which.min(colMeans(W[,subcomps]))])
+      if(length(  subcomps)>=2){
+        wisped=semiSupNMF:::.qWISP(expdat,W[,subcomps],scaling="none",sum_LessThanOne=T)
+      }
+    },silent=T)
+  }
+
+  if(is.null(wisped)){  return(NULL) }
+
+  return(
+    list(
+      prop  =wisped[,subcomps],
+      scores=wisped[,setdiff(colnames(wisped),subcomps)]
+    )
+  )
+}
+
+.qWISP=function (data, centro, scaling = c("none", "scale", "center")[1],
+                 sum_LessThanOne = TRUE)
+{
+  cutoff_ttest_weights=0.05
+  g = intersect(rownames(data), rownames(centro))
+  data = data[g, ]
+  centro = as.matrix(centro[g, ])
+  if (scaling == "scale") {
+    data = scale(data, scale = TRUE)
+    centro = scale(centro, scale = TRUE)
+  }
+  if (scaling == "center") {
+    data = scale(data, scale = FALSE)
+    centro = scale(centro, scale = FALSE)
+  }
+  nJ = ncol(centro)
+  A = centro
+  nSubj = ncol(data)
+  mixCoef = matrix(0, nSubj, nJ)
+  rownames(mixCoef) = colnames(data)
+  colnames(mixCoef) = colnames(centro)
+  Amat = cbind(rep(-1, nJ), diag(nJ))
+  b0vec = c(-1, rep(0, nJ))
+  if (sum_LessThanOne == TRUE) {
+    meq = 0
+  }
+  else {
+    meq = 1
+  }
+  output = data.frame(t(apply(data, 2, function(y) {
+    obs = which(!is.na(y))
+    Dmat = t(centro[obs, ]) %*% centro[obs, ]
+    diag(Dmat) <- diag(Dmat) + 1e-08
+    mixCoef = quadprog::solve.QP(Dmat, t(centro[obs, ]) %*%
+                                   y[obs], Amat, b0vec, meq = meq)$sol
+    B = as.matrix(y)
+    coeff = round(mixCoef, 4)
+    names(coeff) = colnames(centro)
+    vBeta = matrix(coeff)
+    dSigmaSq = sum((B - A %*% vBeta)^2)/(nrow(A) - ncol(A))
+    dTotalSq = sum((B)^2)/(nrow(A))
+    dModelSq = sum((A %*% vBeta)^2)/(ncol(A))
+    mVarCovar = try(dSigmaSq * chol2inv(chol((t(A) %*% A)+10^-8 )))
+    Adjusted.R.squared = round((dTotalSq - dSigmaSq)/dTotalSq,
+                               3)
+    Ftest = dModelSq/dSigmaSq
+    p.Ftest = stats::pf(q = Ftest, df1 = ncol(A), df2 = nrow(A) -
+                          ncol(A), lower.tail = FALSE)
+    ng = nrow(centro)
+    if (!is.character(mVarCovar)) {
+      vStdErr = sqrt(diag(mVarCovar))
+      CI.inf = sapply(1:nJ, function(j) {
+        round(coeff[j] - (stats::qt(1 - cutoff_ttest_weights/2,
+                                    ng - nJ) * vStdErr[j]), 2)
+      })
+      CI.sup = sapply(1:nJ, function(j) {
+        round(coeff[j] + (stats::qt(1 - cutoff_ttest_weights/2,
+                                    ng - nJ) * vStdErr[j]), 2)
+      }
+      )
+      tvalue = sapply(1:nJ, function(j) {
+        coeff[j]/vStdErr[j]
+      })
+      p.Ttest = sapply(1:nJ, function(j) {
+        stats::pt(abs(coeff[j]/vStdErr[j]), df = ng -
+                    nJ, lower = FALSE) * 2
+      }
+      )
+    }
+    else {
+      mVarCovar = NA
+      vStdErr = CI.inf = CI.sup = p.Ttest = rep(NA, nJ)
+    }
+    names(CI.inf) = paste("CI.inf.", colnames(centro), sep = "")
+    names(CI.sup) = paste("CI.sup.", colnames(centro), sep = "")
+    names(p.Ttest) = paste("Pvalue.", colnames(centro), sep = "")
+    coeff.filtered = coeff
+    coeff.filtered[p.Ttest > cutoff_ttest_weights] = 0
+    names(coeff.filtered) = paste(names(coeff), ".filtered",
+                                  sep = "")
+    dist.Obs.Model = round(sqrt(sum(((centro %*% coeff) -
+                                       y)^2)), 2)
+    c(coeff, dist.Obs.Model = dist.Obs.Model, Ftest.pvalue = p.Ftest,
+      Adjusted.R.squared = Adjusted.R.squared)
+  }
+  )))
+  output$topWeightedClass = colnames(centro)[apply(output[,1:nJ], 1, which.max)]
+  output$deltaTopWeights = apply(output[, 1:nJ], 1, function(z) abs(diff(sort(z,
+                                                                              decreasing = TRUE)[1:2]))
+  )
+
+  return(output)
+}
 
 
 
